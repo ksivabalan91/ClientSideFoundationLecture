e14b8ad86b01473faaf5c3b9ede206cdec63f10a
K.Sivabalan <ksivabalan.91@gmail.com>
2023-04-17 12:35:26 +0800

    CSF day 31

---
+++ angluarcommands.sh
@@ -0,0 +1,8 @@
+# create app
+ng new <app name>
+
+# install modules
+npm install -save <module>
+
+# to run the app
+ng serve -o 
---
+++ day31app.sh
@@ -0,0 +1,10 @@
+ng new day-31-lecture
+# dont add angular routing
+# use CSS
+
+# generate components directory and add a component
+ng generate component components/my-component
+# or short form
+ng g c components/my-component
+# to exclude additional "my-component" folders from breing created
+ng g c components/my-component --skip-tests --flat
