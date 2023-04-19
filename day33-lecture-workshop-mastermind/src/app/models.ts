d31de1b5a3f32a0499a46046dc8c727e4856f2d2
K.Sivabalan <ksivabalan.91@gmail.com>
2023-04-19 15:54:30 +0800

    mastermind update

---
+++ day33-lecture-workshop-mastermind/src/app/models.ts
@@ -0,0 +1,8 @@
+export interface Guess {
+  color1: 'R' | 'B' | 'G' | 'Y' | 'O' | 'P';
+  color2: 'R' | 'B' | 'G' | 'Y' | 'O' | 'P';
+  color3: 'R' | 'B' | 'G' | 'Y' | 'O' | 'P';
+  color4: 'R' | 'B' | 'G' | 'Y' | 'O' | 'P';
+}
+
+
