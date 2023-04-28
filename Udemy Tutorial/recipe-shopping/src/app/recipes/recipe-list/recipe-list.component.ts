c8ad89e733b645dc0b8117e33dee2b0c27c68743
ksivabalan91 <ksivabalan.91@gmail.com>
2023-04-28 17:06:40 +0800

    working recipe list

--- Udemy Tutorial/recipe-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Udemy Tutorial/recipe-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,13 +9,19 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit{
   recipes: Recipe[] = [
     new Recipe('Cajun chicken','It is spicy as fk. Not for the faint of heart','https://rasamalaysia.com/wp-content/uploads/2018/09/cajun-chicken-thumb.jpg'),
-    new Recipe('cajun chicken','spicy as fk','https://rasamalaysia.com/wp-content/uploads/2018/09/cajun-chicken-thumb.jpg')
+    new Recipe('The perfect cheeseburger',"It’s a fast-food classic but not all cheeseburgers are created equal.",'https://images.immediate.co.uk/production/volatile/sites/2/2020/04/Cheeseburger-74e8cde.jpg?quality=90&resize=556,505')
   ];
+
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
 
   ngOnInit(){
   }
 
   constructor(){}
-
+  
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 
 }
