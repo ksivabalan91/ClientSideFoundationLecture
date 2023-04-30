a2a35360b33e2cdf0dcf2e7a5cc74ffd7dd49247
ksivabalan91 <ksivabalan.91@gmail.com>
2023-04-30 09:24:57 +0800

    servcices

---
+++ Udemy Tutorial/services-depedency-injection/src/app/logging.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggingService {
+  logStatusChange(status:string){
+    console.log('A server status has changed, new status: '+status);
+  }
+
+constructor() { }
+
+}
